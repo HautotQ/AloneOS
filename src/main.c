5afc131ba09d24582ad2114f90372440c9c4f684
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-15 11:13:49 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "kernel/kernel.h"
+#include "ui/ui.h"
+#include "platform/macos_platform.h"
+
+int main() {
+    platform_init();
+    ui_init();
+    kernel_init();
+
+    // Boucle principale simulée
+    for (int i = 0; i < 3; i++) {
+        kernel_tick();
+    }
+
+    return 0;
+}
