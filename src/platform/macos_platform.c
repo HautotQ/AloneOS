073a1c5c653e8fcf6e95a7ead54b8fa8f3396779
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-15 11:02:36 +0100

    Create macos_platform.c

---
+++ src/platform/macos_platform.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "macos_platform.h"
+
+// Init plateforme spécifique
+void platform_init() {
+    printf("[Platform] AloneOS tourne sur macOS (hôte)\n");
+}
+
+// Fonctions spécifiques à macOS peuvent être ajoutées ici
